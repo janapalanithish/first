3231394e2a581329c1b11e947e6cff11daafa5f4
nithish kumar <janapalanithish1982715@gmail.com>
2026-03-30 19:48:01 +0530

    css

--- frontend/scriptmslj.js
+++
@@ -1,8 +0,0 @@
-let h2 = document.querySelector("h2");
-let text = h2.innerText
-h2.innerText = h2.innerText + "with apna college";
-console.dir = document.querySelector("h2").innerText;
-let div = document.querySelectorAll("div");
-div.innertext="this";
-let divs =  document.querySelector(".box").innerText="nithish kumar";
- console.log(divs)
